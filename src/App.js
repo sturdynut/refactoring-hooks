27efa8d1456f5ca1339d2f05495b51920691df73
Matti Salokangas <matti.salokangas@codingzeal.com>
2020-09-04 16:44:17 -0700

    ✨ Show size range slider and update image by selected size
    - Add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+const defaults = {
+  url: "https://picsum.photos/id/1084/536/354?grayscale",
+  size: 200
+}
+
+const getUrl = size => `https://picsum.photos/${size}`
 
 function App() {
+  const [size, setSize] = React.useState(defaults.size);
+  const [url, setUrl] = React.useState(defaults.url);
+
+  const handleChange = ({ target }) => {
+    setUrl(getUrl(target.value));
+    setSize(target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container items-center justify-center">
+       <div className="flex flex-col">
+         <div className="flex flex-col">
+            <h1 className="font-4xl">Random Image by Size</h1>
+            <div className="font-thin text-gray-600">Current size is {size}</div>
+            <input type="range" className="bg-white rounded-lg shadow-xl" min={50} max={500} value={size} onChange={handleChange} />
+         </div>
+         <div>
+           <img src={url} alt="" className="bg-white rounded-lg shadow-xl mt-8" />
+         </div>
+       </div>
     </div>
   );
 }
